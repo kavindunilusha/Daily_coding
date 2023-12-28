e7d3c63e33647b0d4e3ff2dd95a582e5b1760902
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2023-12-29 00:04:42 +0530

    Add files via upload

---
+++ ques03/EvenOddPrinter.java
@@ -0,0 +1,74 @@
+package ques03;
+
+public class EvenOddPrinter {
+	
+	private static final int MAX_NUM = 20;
+	private static volatile int nextNum = 1;
+	private static final Object lock = new Object();
+
+	public static void main(String[] args) {
+		
+		Thread evenThread = new Thread(new EvenRunnable() , "EvenThread") ;
+		Thread oddThread = new Thread(new OddRunnable() , "OddThread") ;
+		
+		evenThread.start();
+		oddThread.start();
+		
+		try {
+			evenThread.join();
+			oddThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	static class EvenRunnable implements Runnable{
+		public void run() {
+			while(nextNum<=MAX_NUM) {
+				synchronized (lock) {
+					if(nextNum % 2 == 0) {
+						System.out.println(Thread.currentThread().getName()+":"+nextNum);
+						nextNum++;
+						lock.notify();
+						
+					}
+					else {
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	static class OddRunnable implements Runnable{
+		
+		public void run() {
+			while(nextNum<=MAX_NUM) {
+				synchronized (lock) {
+					if(nextNum % 2 == 1) {
+						System.out.println(Thread.currentThread().getName()+":"+nextNum);
+						nextNum++;
+						lock.notify();
+						
+					}
+					else {
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+		
+	}
+
+}
