65fb34b17c4fff8622298826de744bc3d7ab84b7
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2023-12-29 00:04:28 +0530

    Add files via upload

---
+++ qu02b/MainApp.java
@@ -0,0 +1,25 @@
+package qu02b;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Pair<String,Integer> pair1 = new Pair<>("Test 1",42);
+		Pair<Double,String> pair2 = new Pair<>(3.14,"Test 2");
+		
+		
+		System.out.println("Printing First Pair");
+		System.out.println("First value:" +pair1.getFirst());
+		System.out.println("Second value:" + pair1.getSecond());
+		
+		System.out.println();
+		
+		System.out.println("Printing Second Pair");
+		System.out.println("First value:" +pair2.getFirst());
+		System.out.println("Second value:" + pair2.getSecond());
+		
+		
+		
+
+	}
+
+}
---
+++ qu02b/Pair.java
@@ -0,0 +1,28 @@
+package qu02b;
+
+public class Pair<T,U> {
+	private T first;
+	private U second;
+	
+	
+	//parameterized constructor
+	public Pair(T first, U second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
+	
+	
+	//method of get first value of pair
+
+
+	public T getFirst() {
+		return first;
+	}
+	
+	//method of get first value of pair
+	public U getSecond() {
+		return second;
+	}
+
+}
