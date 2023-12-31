d6a6c45f6ddad67ee0b7c3ad1b8f9f808acc5288
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2023-12-31 18:00:15 +0530

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,30 @@
+package Question01;
+
+public class Cat extends Pet {
+	
+	@Override
+	public void GetTotalPoints() {
+		System.out.println("Total points is " + getters());
+		
+		
+	}
+	
+	public void feed() {
+		System.out.println("feeding the Cat");
+		setter(getters()+5);
+	}
+
+	public void clean() {
+
+		System.out.println("cleanig the Cat");
+		setter(getters() + 10);
+	}
+
+	public void cuddle() {
+
+		System.out.println("cuddling the Cat");
+		setter(getters() + 15);
+
+	}
+
+}
